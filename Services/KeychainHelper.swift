701a91d4112a254508a17a397f738a13a07597f1
RUTHVIK CHINTAPALLI <150786097+ruthvikchintapalli@users.noreply.github.com>
2025-11-09 02:40:49 -0600

    Implement KeychainHelper for secure data storage

---
+++ Services/KeychainHelper.swift
@@ -0,0 +1,32 @@
+import Security
+
+class KeychainHelper {
+    func save(_ key: String, data: Data) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
+        SecItemDelete(query as CFDictionary)
+        SecItemAdd(query as CFDictionary, nil)
+    }
+
+    func get(_ key: String) -> Data? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: true
+        ]
+        var result: AnyObject?
+        SecItemCopyMatching(query as CFDictionary, &result)
+        return result as? Data
+    }
+
+    func delete(_ key: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
