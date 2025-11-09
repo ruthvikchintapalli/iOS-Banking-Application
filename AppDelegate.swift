16c1ed83f248fa99954d3c29485a3d18af847a8e
RUTHVIK CHINTAPALLI <150786097+ruthvikchintapalli@users.noreply.github.com>
2025-11-09 02:45:51 -0600

    Add AppDelegate for Firebase setup and notifications

---
+++ AppDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        MessagingService.shared
+        return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+}
